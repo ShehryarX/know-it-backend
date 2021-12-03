d1bad21fafeb02c2c8dcb659b2d0be3f632020c4
Shehryar Assad <xshehryar@gmail.com>
2021-12-02 23:55:19 -0500

    temp

---
+++ script.sh
@@ -0,0 +1,377 @@
+
+COMMIT_HASH="8e4f452"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Thu Dec 02 2021 20:54:20 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="2c1ac4a"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Thu Dec 02 2021 17:43:28 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="72b1c85"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Tyler Pinto <tjpinto@uwaterloo.ca>"
+git commit --amend --no-edit --date "Thu Dec 02 2021 12:21:57 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="1afc43f"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Thu Dec 02 2021 09:48:24 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="38aa7a6"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Thu Dec 02 2021 08:58:58 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="6751b5b"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Thu Dec 02 2021 04:52:32 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="59ab1fb"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Wed Dec 01 2021 18:50:12 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="839590f"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Hussain Abdi <hussainabdi12@gmail.com>"
+git commit --amend --no-edit --date "Wed Dec 01 2021 13:47:20 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="363298b"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Tyler Pinto <tjpinto@uwaterloo.ca>"
+git commit --amend --no-edit --date "Wed Dec 01 2021 11:32:04 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="d301f19"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Tyler Pinto <tjpinto@uwaterloo.ca>"
+git commit --amend --no-edit --date "Wed Dec 01 2021 10:28:37 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="2188f4c"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Wed Dec 01 2021 01:59:03 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="ae079c6"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Tue Nov 30 2021 20:57:48 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="6e7daf4"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Hussain Abdi <hussainabdi12@gmail.com>"
+git commit --amend --no-edit --date "Tue Nov 30 2021 11:07:19 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="66399da"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Tyler Pinto <tjpinto@uwaterloo.ca>"
+git commit --amend --no-edit --date "Tue Nov 30 2021 09:51:08 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="4da4f54"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Hussain Abdi <hussainabdi12@gmail.com>"
+git commit --amend --no-edit --date "Tue Nov 30 2021 04:20:46 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="88be027"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Tue Nov 30 2021 00:45:33 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="aa877ed"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Mon Nov 29 2021 22:42:18 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="e2a9679"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Mon Nov 29 2021 21:42:44 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="60ee0f4"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Mon Nov 29 2021 16:46:55 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="1486c66"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Mon Nov 29 2021 15:58:47 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="5228cd7"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Tyler Pinto <tjpinto@uwaterloo.ca>"
+git commit --amend --no-edit --date "Mon Nov 29 2021 07:20:39 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="766e2a4"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Mon Nov 29 2021 07:17:18 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="b44a72a"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Sun Nov 28 2021 23:34:11 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="09452fb"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Sun Nov 28 2021 21:52:17 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="924e303"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Advitya Chhabra <advitya10@hotmail.com>"
+git commit --amend --no-edit --date "Sun Nov 28 2021 08:52:31 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="ed9d9f9"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Advitya Chhabra <advitya10@hotmail.com>"
+git commit --amend --no-edit --date "Sun Nov 28 2021 03:38:09 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="dac0ef9"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Sun Nov 28 2021 01:51:58 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="feb3f1b"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Sat Nov 27 2021 18:58:59 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="8fd7705"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Sat Nov 27 2021 16:40:41 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="156958c"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Sat Nov 27 2021 09:40:57 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="15c31f2"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Fri Nov 26 2021 21:33:44 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="cbea4d8"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Fri Nov 26 2021 20:16:58 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="26e5161"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Advitya Chhabra <advitya10@hotmail.com>"
+git commit --amend --no-edit --date "Fri Nov 26 2021 17:07:30 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="d8aa428"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Fri Nov 26 2021 00:42:00 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="c01ebf3"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Thu Nov 25 2021 22:14:40 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="07f75f6"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Thu Nov 25 2021 18:41:02 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="531a87c"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Hussain Abdi <hussainabdi12@gmail.com>"
+git commit --amend --no-edit --date "Thu Nov 25 2021 16:51:30 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="cd6c6a0"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Thu Nov 25 2021 13:19:21 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="0a3de5a"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Hussain Abdi <hussainabdi12@gmail.com>"
+git commit --amend --no-edit --date "Thu Nov 25 2021 12:11:43 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="5ad99c8"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Hussain Abdi <hussainabdi12@gmail.com>"
+git commit --amend --no-edit --date "Thu Nov 25 2021 02:37:04 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="e8a9e5f"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Wed Nov 24 2021 21:19:59 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="37ec679"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Wed Nov 24 2021 21:19:35 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="4a28c93"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shahbaz Momi <ssmomi@uwaterloo.ca>"
+git commit --amend --no-edit --date "Wed Nov 24 2021 20:32:16 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="7f45c37"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Wed Nov 24 2021 17:50:16 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="44967c7"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Rafit Jamil <rafit.jamil@gmail.com>"
+git commit --amend --no-edit --date "Wed Nov 24 2021 16:04:17 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="a401437"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Tyler Pinto <tjpinto@uwaterloo.ca>"
+git commit --amend --no-edit --date "Tue Nov 23 2021 11:56:13 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
+COMMIT_HASH="4e2734c"
+git checkout $COMMIT_HASH
+git commit --amend --no-edit --author "Shehryar Assad <xshehryar@gmail.com>"
+git commit --amend --no-edit --date "Tue Nov 23 2021 11:03:34 GMT-0500 (Eastern Standard Time)"
+NEW_COMMIT_HASH=$(git log -1 --pretty=format:"%h")
+git checkout master
+git replace -f $COMMIT_HASH $NEW_COMMIT_HASH
+    
